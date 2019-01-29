a09ee1f1d28db1b0bff8a8cd514a6ec1bb38b30d
Robert Lancaster <rlancaste@gmail.com>
2019-01-29 15:10:47 -0500

    Adding craft install if removeall is desired

--- build-kstars.sh
+++ build-kstars.sh
@@ -202,6 +202,8 @@ EOF
 				exit
 			fi
 			rm -rf "${CRAFT_DIR}"
+			mkdir -p ${CRAFT_DIR}
+			curl https://raw.githubusercontent.com/KDE/craft/master/setup/CraftBootstrap.py -o setup.py && python3.7 setup.py --prefix "${CRAFT_DIR}"
 		fi
 	else
 		announce "Installing craft"
