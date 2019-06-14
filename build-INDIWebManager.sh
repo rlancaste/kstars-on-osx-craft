013bac763af553fe58be677352a48b873fd40e30
Robert Lancaster <rlancaste@gmail.com>
2019-06-14 00:54:18 -0400

    Adding files for INDIWebManagerApp

---
+++ build-INDIWebManager.sh
@@ -0,0 +1,357 @@
+#!/bin/bash
+
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+ANNOUNCE=""
+BUILD_INDI=""
+STABLE_BUILD=""
+GENERATE_DMG=""
+#GENERATE_XCODE=""
+FORCE_RUN=""
+INDI_WEB_MANAGER_APP=""
+REMOVE_ALL=""
+VERBOSE=""
+
+#This will print out how to use the script
+function usage
+{
+
+cat <<EOF
+	options:
+	    -a Announce stuff as you go
+	    -s Build the latest stable release (the default is to build the latest version from git)
+	    -d Generate dmg
+	    -f Force build even if there are script updates
+	    -r Remove everything and do a fresh install
+	    -v Print out verbose output while building
+	    -q Craft is in quiet mode while building
+EOF
+}
+
+#This function prints the usage information if the user enters an invalid option or no option at all and quits the program 
+	function dieUsage
+	{
+		echo ""
+		echo $*
+		echo ""
+		usage
+		exit 9
+	}
+
+#These functions are involved in quitting the script
+	function exitEarly
+	{
+		announce "$*"
+		trap - EXIT
+		exit 0
+	}
+	
+	function scriptDied
+	{
+    	announce "Something failed"
+	}
+
+#This function processes the user's options for running the script
+	function processOptions
+	{
+		while getopts "adfrvsq" option
+		do
+			case $option in
+				a)
+					ANNOUNCE="Yep"
+					;;
+				s)
+					STABLE_BUILD="Yep"
+					;;
+				d)
+					GENERATE_DMG="Yep"
+					;;
+				#x)
+				#	GENERATE_XCODE="Yep"
+				#	;;
+				f)
+					FORCE_RUN="Yep"
+					;;
+				r)
+					REMOVE_ALL="Yep"
+					;;
+				v)
+					VERBOSE="-vvv"
+					;;
+				q)
+					VERBOSE="-q"
+					;;
+				*)
+					dieUsage "Unsupported option $option"
+					;;
+			esac
+		done
+		shift $((${OPTIND} - 1))
+
+		echo ""
+		echo "ANNOUNCE           = ${ANNOUNCE:-Nope}"
+		echo "STABLE_BUILD       = ${STABLE_BUILD:-Nope}"
+		echo "GENERATE_DMG       = ${GENERATE_DMG:-Nope}"
+		#echo "GENERATE_XCODE     = ${GENERATE_XCODE:-Nope}"
+		echo "FORCE_RUN          = ${FORCE_RUN:-Nope}"
+		echo "REMOVE_ALL         = ${REMOVE_ALL:-Nope}"
+		echo "VERBOSE            = ${VERBOSE:-Nope}"
+	}
+
+# This function checks to see if a connection to a website exists.
+#
+	function checkForConnection
+	{
+		testCommand=$(curl -Is $2 | head -n 1)
+		if [[ "${testCommand}" == *"OK"* || "${testCommand}" == *"Moved"* ]]
+  		then 
+  			echo "$1 connection was found."
+  		else
+  			echo "$1, ($2), a required connection, was not found, aborting script."
+  			echo "If you would like the script to run anyway, please comment out the line that tests this connection in build-INDIWebManager.sh."
+  			exit
+		fi
+	}
+
+#This checks to see that this script is up to date.  If it is not, you can use the -f option to force it to run.
+	function checkUpToDate
+	{	
+		cd "$DIR"
+
+		localVersion=$(git log --pretty=%H ...refs/heads/master^ | head -n 1)
+		remoteVersion=$(git ls-remote origin -h refs/heads/master | cut -f1)
+		cd - > /dev/null
+		echo ""
+		echo ""
+
+		if [ "${localVersion}" != "${remoteVersion}" ]
+		then
+
+			if [ -z "$FORCE_RUN" ]
+			then
+				echo "Script is out of date"
+				echo ""
+				echo "override with a -f"
+				echo ""
+				echo "There is a newer version of the script available, please update - run"
+				echo "cd $DIR ; git pull"
+
+				echo "Aborting run"
+				exit 9
+			else
+				echo "WARNING: Script is out of date"
+			
+				echo "Forcing run"
+			fi
+		else
+			echo "Script is up-to-date"
+			echo ""
+		fi	
+	}
+
+
+########################################################################################
+# This is where the main part of the script starts!
+#
+
+#Process the command line options to determine what to do.
+	processOptions $@
+	
+#Check to see that this script is up to date.  If you want it to run anyway, use the -f option.
+	checkUpToDate
+	
+# Prepare to run the script by setting all of the environment variables	
+	source ${DIR}/build-env.sh
+	
+# Before starting, check to see if the remote servers are accessible
+	statusBanner "Checking Connections"
+	checkForConnection Homebrew "https://raw.githubusercontent.com/Homebrew/install/master/install"
+	checkForConnection INDIWebSrc "https://github.com/rlancaste/INDIWebManagerApp.git"
+	checkForConnection Oxygen "https://github.com/KDE/oxygen.git"
+	checkForConnection CustomMacBlueprints "https://github.com/rlancaste/craft-blueprints-kde.git"
+	
+#Announce the script is starting and what will be done.
+	announce "Starting script, building INDI and INDIWebManager with Craft"
+	if [ -n "$GENERATE_DMG" ]
+	then
+		announce "and then building a DMG"
+	fi
+	
+# This installs the xcode command line tools if not installed yet.
+# Yes these tools will be automatically installed if the user has never used git before
+# But sometimes they need to be installed again.
+# If they are already installed, it will just print an error message
+	announce "Installing xcode command line tools"
+	xcode-select --install
+	
+# From here on out exit if there is a failure
+	set -e
+	trap scriptDied EXIT
+	
+#This will install homebrew if it hasn't been installed yet, or reset homebrew if desired.
+	if [ -d "/usr/local/Homebrew" ]
+	then
+		#This will remove all the homebrew packages if desired.
+		if [ -n "$REMOVE_ALL" ]
+		then
+			announce "You have selected the REMOVE_ALL option.  Warning, this will clear all currently installed homebrew packages."
+			read -p "Do you really wish to proceed? (y/n)" runscript
+			if [ "$runscript" != "y" ]
+			then
+				echo "Quitting the script as you requested."
+				exit
+			fi
+			brew remove --force $(brew list) --ignore-dependencies
+		fi  
+	else
+		announce "Installing Homebrew."
+		/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+	fi
+
+#This will install INDI_WEB_MANAGER dependencies from Homebrew.
+	announce "Installing Homebrew Dependencies."
+	brew upgrade
+	
+	# python is required for craft to work.
+	brew install python
+	
+	# Craft does build ninja and install it to the craft directory, but QT Creator expects the homebrew version.
+	brew install ninja
+	
+	# I tried to write a recipe for gpsd, but it requires scons, and I have no idea what to do.
+	brew install gpsd 
+	
+	# It would be good to sort this out.  gpg2 should be built in craft.  TODO!
+	brew install gpg
+	
+	# This is because gpg is not called gpg2 and translations call on gpg2.  Fix this??
+	ln -sf /usr/local/bin/gpg /usr/local/bin/gpg2
+	
+	# It would be good to get this stuff into craft too!!! TODO!
+	# The problem here is that the system ruby can't be changed and we need logger-colors.
+	brew install ruby
+	export PATH=/usr/local/opt/ruby/bin:$PATH
+	gem install logger-colors
+
+#This will create the Astro Directory if it doesn't exist
+	mkdir -p "${ASTRO_ROOT}"
+
+#This will install craft if it is not installed yet.  It will clear the old one if the REMOVE_ALL option was selected.
+	if [ -d "${CRAFT_DIR}" ]
+	then
+		#This will remove the current craft if desired.
+		if [ -n "$REMOVE_ALL" ]
+		then
+			announce "You have selected the REMOVE_ALL option.  Warning, this will clear the entire craft directory."
+			read -p "Do you really wish to proceed? (y/n)" runscript2
+			if [ "$runscript2" != "y" ]
+			then
+				echo "Quitting the script as you requested."
+				exit
+			fi
+			if [ -d "${CRAFT_DIR}" ]
+			then
+				rm -rf "${CRAFT_DIR}"
+			fi
+			mkdir -p ${CRAFT_DIR}
+			curl https://raw.githubusercontent.com/KDE/craft/master/setup/CraftBootstrap.py -o setup.py && python3.7 setup.py --prefix "${CRAFT_DIR}"
+		fi
+	else
+		announce "Installing craft"
+		mkdir -p ${CRAFT_DIR}
+		curl https://raw.githubusercontent.com/KDE/craft/master/setup/CraftBootstrap.py -o setup.py && python3.7 setup.py --prefix "${CRAFT_DIR}"
+	fi  
+	
+#This copies all the required craft settings
+	statusBanner "Copying Craft Settings and Blueprint settings specific to building on macs."
+	cp ${DIR}/CraftSettings.ini ${CRAFT_DIR}/etc/
+	cp ${DIR}/BlueprintSettings.ini ${CRAFT_DIR}/etc/
+	statusBanner "Replacing default craft recipes with revised Mac recipes until they get revised and accepted."
+	rm -rf ${CRAFT_DIR}/etc/blueprints/locations/craft-blueprints-kde
+	cd ${CRAFT_DIR}/etc/blueprints/locations
+	git clone https://github.com/rlancaste/craft-blueprints-kde.git
+
+#This will build indi, including the 3rd Party drivers.
+	announce "Building INDI and required dependencies"
+	source ${CRAFT_DIR}/craft/craftenv.sh
+	
+	if [ -n "$STABLE_BUILD" ]
+	then
+		TARGET_VER="default"
+	else
+		TARGET_VER="Latest"
+	fi
+	
+	craft "$VERBOSE" -i --target "${TARGET_VER}" indiserver
+	
+	announce "Building INDI 3rd Party Libraries and required dependencies"
+	craft "$VERBOSE" -i --target "${TARGET_VER}" indiserver3rdPartyLibraries
+	
+	announce "Building INDI 3rd Party Drivers"
+	craft "$VERBOSE" -i --target "${TARGET_VER}" indiserver3rdParty
+
+#This will build gsc
+	#announce "Building GSC"
+	#craft "$VERBOSE" gsc
+	
+#This will set the INDI_WEB_MANAGER App directory and craft INDI_WEB_MANAGER.
+	announce "Building INDIWebManager and required dependencies"
+	export INDI_WEB_MANAGER_APP="${CRAFT_DIR}/Applications/KDE/INDIWebManager.app"
+	if [ -d "${INDI_WEB_MANAGER_APP}" ]
+	then
+		rm -rf "${INDI_WEB_MANAGER_APP}"
+	fi
+	
+	source ${CRAFT_DIR}/craft/craftenv.sh
+		
+	statusBanner "Crafting icons"
+	craft "$VERBOSE" breeze-icons
+		
+	statusBanner "Crafting INDIWebManager"
+	craft "$VERBOSE" -i --target "${TARGET_VER}" indiwebmanagerapp-mac
+		
+	announce "CRAFT COMPLETE"
+	
+#This will create some symlinks that make it easier to edit INDI and INDIWebManager
+	announce "Creating symlinks"
+	mkdir -p ${SHORTCUTS_DIR}
+	
+	if [ -d ${SHORTCUTS_DIR} ]
+	then
+		rm -f ${SHORTCUTS_DIR}/*
+	fi
+	
+	#Craft Shortcuts
+	ln -sf ${CRAFT_DIR}/Applications/KDE ${SHORTCUTS_DIR}
+	ln -sf ${CRAFT_DIR}/bin ${SHORTCUTS_DIR}
+	ln -sf ${CRAFT_DIR}/lib ${SHORTCUTS_DIR}
+	ln -sf ${CRAFT_DIR}/include ${SHORTCUTS_DIR}
+	ln -sf ${CRAFT_DIR}/share ${SHORTCUTS_DIR}
+	ln -sf ${CRAFT_DIR}/etc/blueprints/locations/craft-blueprints-kde ${SHORTCUTS_DIR}
+	
+	# INDIWebManager
+	ln -sf ${CRAFT_DIR}/download/git/kde/applications/indiwebmanagerapp-mac ${SHORTCUTS_DIR}
+	mv ${SHORTCUTS_DIR}/indiwebmanagerapp-mac ${SHORTCUTS_DIR}/indiwebmanager-source
+	ln -sf ${CRAFT_DIR}/build/kde/applications/indiwebmanagerapp-mac/work/RelWithDebInfo-Latest ${SHORTCUTS_DIR}
+	mv ${SHORTCUTS_DIR}/RelWithDebInfo-Latest ${SHORTCUTS_DIR}/indiwebmanagerapp-build
+	
+	# INDIServer
+	ln -sf ${CRAFT_DIR}/download/git/libs/indiserver ${SHORTCUTS_DIR}
+	mv ${SHORTCUTS_DIR}/indiserver ${SHORTCUTS_DIR}/indiserver-source
+	ln -sf ${CRAFT_DIR}/build/libs/indiserver/work/RelWithDebInfo-Latest ${SHORTCUTS_DIR}
+	mv ${SHORTCUTS_DIR}/RelWithDebInfo-Latest ${SHORTCUTS_DIR}/indiserver-build
+	
+	# INDIServer 3rdParty
+	ln -sf ${CRAFT_DIR}/download/git/libs/indiserver3rdParty ${SHORTCUTS_DIR}
+	mv ${SHORTCUTS_DIR}/indiserver3rdParty ${SHORTCUTS_DIR}/indiserver-3rdParty-source
+	ln -sf ${CRAFT_DIR}/build/libs/indiserver3rdParty/work/RelWithDebInfo-Latest ${SHORTCUTS_DIR}
+	mv ${SHORTCUTS_DIR}/RelWithDebInfo-Latest ${SHORTCUTS_DIR}/indiserver-3rdParty-build
+
+#This will package everything up into the app and then make a dmg.
+	if [ -n "$GENERATE_DMG" ]
+	then
+		source ${DIR}/generate-dmg-INDIWebManager.sh
+	fi
+# Finally, remove the trap
+	trap - EXIT
+	announce "Script execution complete"
---
+++ fix-libraries-INDIWebManager.sh
@@ -0,0 +1,226 @@
+#/bin/bash
+
+# This script has three goals:
+# 1) It makes sure the DMG folder is set up, INDIWebManagerApp is copied there, and the variables aree correct.
+# 2) identify programs that use libraries outside of the package (that meet certain criteria)
+# 3) copy those libraries to the blah/Frameworks dir
+# 4) Update those programs to know where to look for said libraries
+
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+#This adds a file to the list so it can be copied to Frameworks
+	function addFileToCopy
+	{
+		for e in "${FILES_TO_COPY[@]}"
+		do 
+			if [ "$e" == "$1" ]
+			then
+				return 0
+			fi
+		done
+	
+		FILES_TO_COPY+=($1)
+	}
+
+#This Function processes a given file using otool to see what files it is using
+#Then it uses install_name_tool to change that target to be a file in Frameworks
+#Finally, it adds the file that it changed to the list of files to copy there.
+	function processTarget
+	{
+		target=$1
+		
+		#This hard coded rpath needs to be removed from any files that have it for packaged apps because later there could be rpath conflicts
+        #if the program is run on a computer with the same paths as the build computer
+        install_name_tool -delete_rpath ${CRAFT_DIR}/lib $file
+        	
+		entries=$(otool -L $target | sed '1d' | awk '{print $1}' | egrep -v "$IGNORED_OTOOL_OUTPUT")
+		echo "Processing $target"
+	
+		relativeRoot="${INDI_WEB_MANAGER_APP}/Contents"
+	
+		pathDiff=${target#${relativeRoot}*}
+
+		#This is a Framework file
+		if [[ "$pathDiff" == /Frameworks/* ]]
+		then
+			newname="@rpath/$(basename $target)"
+			install_name_tool -add_rpath "@loader_path/" $file		
+			echo "    This is a Framework, change its own id $target -> $newname" 
+			
+			install_name_tool -id \
+			$newname \
+			$target
+		else
+		    pathToFrameworks=$(echo $(dirname "${pathDiff}") | awk -F/ '{for (i = 1; i < NF ; i++) {printf("../")} }')
+			pathToFrameworks="${pathToFrameworks}Frameworks/"
+			install_name_tool -add_rpath "@loader_path/${pathToFrameworks}" $file
+		fi
+		
+		for entry in $entries
+		do
+			baseEntry=$(basename $entry)
+			newname=""
+			newname="@rpath/${baseEntry}"
+			echo "    change reference $entry -> $newname" 
+
+			install_name_tool -change \
+			$entry \
+			$newname \
+			$target
+
+			addFileToCopy "$entry"
+		done
+		echo ""
+		echo "   otool for $target after"
+		otool -L $target | egrep -v "$IGNORED_OTOOL_OUTPUT" | awk '{printf("\t%s\n", $0)}'
+	
+	}
+
+#This copies all of the files in the list into Frameworks
+	function copyFilesToFrameworks
+	{
+		FILES_COPIED=0
+		for libFile in "${FILES_TO_COPY[@]}"
+		do
+			# if it starts with a / then easy.
+			#
+			base=$(basename $libFile)
+
+			if [[ $libFile == /* ]]
+			then
+				filename=$libFile
+			else
+				# see if I can find it, NOTE:  I had to add the last part and the echo because the find produced multiple results breaking the file copy into frameworks.
+				filename=$(echo $(find "${CRAFT_DIR}/lib" -name "${base}")| cut -d" " -f1)
+				if [[ "$filename" == "" ]]
+				then
+					filename=$(echo $(find /usr/local/lib -name "${base}")| cut -d" " -f1)
+				fi
+			fi    
+
+			if [ ! -f "${FRAMEWORKS_DIR}/${base}" ]
+			then
+				echo "HAVE TO COPY [$base] from [${filename}] to Frameworks"
+				cp -fL "${filename}" "${FRAMEWORKS_DIR}"
+				
+				FILES_COPIED=$((FILES_COPIED+1))
+			
+				# Seem to need this for the macqtdeploy
+				#
+				chmod +w "${FRAMEWORKS_DIR}/${base}"
+		
+			
+			else
+				echo ""
+				echo "Skipping Copy: $libFile already in Frameworks "
+			fi
+		done
+	}
+	
+	function processDirectory
+	{
+		directoryName=$1
+		directory=$2
+		statusBanner "Processing all of the $directoryName files in $directory"
+		FILES_TO_COPY=()
+		for file in ${directory}/*
+		do
+    		base=$(basename $file)
+
+        	statusBanner "Processing $directoryName file $base"
+        	processTarget $file
+        	
+		done
+
+		statusBanner "Copying required files for $directoryName into frameworks"
+		copyFilesToFrameworks
+	}
+	
+	
+	
+#########################################################################
+#This is where the main part of the script starts!!
+#
+
+#This code should only run if the user is running the fix-libraries script without running build-indiwebmanagerapp.sh or generate-dmg.sh
+if [ -z "${ASTRO_ROOT}" ]
+then
+	source ${DIR}/build-env.sh
+fi
+
+#This should stop the script so that it doesn't run if these paths are blank.
+#That way it doesn't try to edit /Applications instead of ${CRAFT_DIR}/Applications for example
+	if [ -z "${DIR}" ] || [ -z  "${CRAFT_DIR}" ]
+	then
+		echo "directory error! aborting Libraries script!"
+		exit 9
+	fi
+
+#This code should make sure the INDI_WEB_MANAGER_APP and the DMG Directory are set correctly.
+#In the case of the CMAKE and XCode builds, it also creates the dmg directory and copies in the app
+	if [ ! -e ${CRAFT_DIR} ]
+	then
+		"INDIWebManager Craft directory does not exist.  You have to build INDIWebManager with Craft first. Use build-INDIWebManagerApp.sh"
+		exit
+	fi
+	mkdir -p "${ASTRO_ROOT}/INDIWebManagerAppDMG"
+	DMG_DIR="${ASTRO_ROOT}/INDIWebManagerAppDMG"
+	cp -rf "${CRAFT_DIR}/Applications/KDE/INDIWebManagerApp.app" "${DMG_DIR}"
+	INDI_WEB_MANAGER_APP="${DMG_DIR}/INDIWebManagerApp.app"
+	
+announce "Running Fix Libraries Script"
+
+	FILES_TO_COPY=()
+	FRAMEWORKS_DIR="${INDI_WEB_MANAGER_APP}/Contents/Frameworks"
+
+#Files in these locations do not need to be copied into the Frameworks folder.
+	IGNORED_OTOOL_OUTPUT="/Qt|${INDI_WEB_MANAGER_APP}/|/usr/lib/|/System/"
+
+#This deletes the former Frameworks folder so you can start fresh.  This is needed if it ran before.
+	statusBanner "Replacing the Frameworks Directory"
+	rm -fr "${FRAMEWORKS_DIR}"
+	mkdir -p "${FRAMEWORKS_DIR}"
+	
+# This deletes the qt.conf file so macdeployqt can create a new one which points inside the app bundle
+	statusBanner "Deleting qt.conf so a new one that points inside the bundle can be made."
+	rm -f "${INDI_WEB_MANAGER_APP}/Contents/Resources/qt.conf"
+
+	
+cd ${DMG_DIR}
+
+statusBanner "Processing INDIWebManager.app executable"
+processTarget "${INDI_WEB_MANAGER_APP}/Contents/MacOS/INDIWebManagerApp"
+
+# Also cheat, and add libindidriver.1.dylib to the list
+#
+addFileToCopy "libindidriver.1.dylib"
+
+statusBanner "Copying first round of files"
+copyFilesToFrameworks
+
+statusBanner "Processing libindidriver library"
+
+# need to process libindidriver.1.dylib
+#
+processTarget "${FRAMEWORKS_DIR}/libindidriver.1.dylib"
+processDirectory indi "${INDI_WEB_MANAGER_APP}/Contents/MacOS/indi"
+
+processDirectory GPHOTO_IOLIBS "${INDI_WEB_MANAGER_APP}/Contents/Resources/DriverSupport/gphoto/IOLIBS"
+processDirectory GPHOTO_CAMLIBS "${INDI_WEB_MANAGER_APP}/Contents/Resources/DriverSupport/gphoto/CAMLIBS"
+
+processDirectory MathPlugins "${INDI_WEB_MANAGER_APP}/Contents/Resources/MathPlugins"
+
+#This should not be necessary because macdeployqt used to do this.  Why do I need to add this?
+processDirectory Platforms "${INDI_WEB_MANAGER_APP}/Contents/PlugIns/platforms"
+
+processDirectory Frameworks "${FRAMEWORKS_DIR}"
+
+while [ ${FILES_COPIED} -gt 0 ]
+do
+	statusBanner "${FILES_COPIED} more files were copied into Frameworks, we need to process it again."
+	processDirectory Frameworks "${FRAMEWORKS_DIR}"
+done
+
+statusBanner "The following files are now in Frameworks:"
+ls -lF ${FRAMEWORKS_DIR}
+
