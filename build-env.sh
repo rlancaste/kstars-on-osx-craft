ec685502d9dff1c775696603f8090490e608dd19
Robert Lancaster <rlancaste@gmail.com>
2019-08-07 22:58:00 -0400

    updating version

--- build-env.sh
+++ build-env.sh
@@ -43,7 +43,7 @@ export QMAKE_MACOSX_DEPLOYMENT_TARGET=10.12
 export MACOSX_DEPLOYMENT_TARGET=10.12
 
 # This sets the current version numbers that will be used throughout the script.
-export KSTARS_VERSION=3.3.3
+export KSTARS_VERSION=3.3.4
 export INDI_WEB_MANAGER_APP_VERSION=1.2
 
 echo "DIR                   is [${DIR}]"
