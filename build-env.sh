d77819eefd391188643b75f2ac2f6b8a391adeb2
Robert Lancaster <rlancaste@gmail.com>
2019-08-09 23:47:11 -0400

    going to version 1.3

--- build-env.sh
+++ build-env.sh
@@ -44,7 +44,7 @@ export MACOSX_DEPLOYMENT_TARGET=10.12
 
 # This sets the current version numbers that will be used throughout the script.
 export KSTARS_VERSION=3.3.4
-export INDI_WEB_MANAGER_APP_VERSION=1.2
+export INDI_WEB_MANAGER_APP_VERSION=1.3
 
 echo "DIR                   is [${DIR}]"
 echo "ASTRO_ROOT            is [${ASTRO_ROOT}]"
